6adfc653440a47be3553377e0868f98a46b454c6
hkucs-makerlab <makerlab@cs.hku.hk>
2020-06-17 22:09:28 +0800

    initial commit

---
+++ Balanduino/src/com/tkjelectronics/balanduino/ImuFragment.java
@@ -0,0 +1,162 @@
+/*************************************************************************************
+ * Copyright (C) 2012-2014 Kristian Lauszus, TKJ Electronics. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * Kristian Lauszus, TKJ Electronics
+ * Web      :  http://www.tkjelectronics.com
+ * e-mail   :  kristianl@tkjelectronics.com
+ *
+ ************************************************************************************/
+
+package com.tkjelectronics.balanduino;
+
+import android.annotation.TargetApi;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class ImuFragment extends Fragment {
+    private Button mButton;
+    public TextView mPitchView;
+    public TextView mRollView;
+    public TextView mCoefficient;
+    private TableRow mTableRow;
+
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable;
+    private int counter = 0;
+    private boolean buttonState;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.imu, container, false);
+
+        mPitchView = (TextView) v.findViewById(R.id.textView1);
+        mRollView = (TextView) v.findViewById(R.id.textView2);
+        mCoefficient = (TextView) v.findViewById(R.id.textView3);
+        mTableRow = (TableRow) v.findViewById(R.id.tableRowCoefficient);
+        mButton = (Button) v.findViewById(R.id.button);
+
+        mHandler.postDelayed(new Runnable() { // Hide the menu icon and tablerow if there is no build in gyroscope in the device
+            @Override
+            public void run() {
+                if (SensorFusion.IMUOutputSelection == -1)
+                    mHandler.postDelayed(this, 100); // Run this again if it hasn't initialized the sensors yet
+                else if (SensorFusion.IMUOutputSelection != 2) // Check if a gyro is supported
+                    mTableRow.setVisibility(View.GONE); // If not then hide the tablerow
+            }
+        }, 100); // Wait 100ms before running the code
+
+        BalanduinoActivity.buttonState = false;
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mHandler.postDelayed(this, 50); // Update IMU data every 50ms
+                if (BalanduinoActivity.mSensorFusion == null)
+                    return;
+                mPitchView.setText(BalanduinoActivity.mSensorFusion.pitch);
+                mRollView.setText(BalanduinoActivity.mSensorFusion.roll);
+                mCoefficient.setText(BalanduinoActivity.mSensorFusion.coefficient);
+
+                counter++;
+                if (counter > 2) { // Only send data every 150ms time
+                    counter = 0;
+                    if (BalanduinoActivity.mChatService == null)
+                        return;
+                    if (BalanduinoActivity.mChatService.getState() == BluetoothChatService.STATE_CONNECTED && BalanduinoActivity.currentTabSelected == ViewPagerAdapter.IMU_FRAGMENT) {
+                        buttonState = mButton.isPressed();
+                        BalanduinoActivity.buttonState = buttonState;
+
+                        if (BalanduinoActivity.joystickReleased || getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) // Check if joystick is released or we are not in landscape mode
+                            CustomViewPager.setPagingEnabled(!buttonState); // Set the ViewPager according to the button
+                        else
+                            CustomViewPager.setPagingEnabled(false);
+
+                        if (BalanduinoActivity.joystickReleased) {
+                            if (buttonState) {
+                                lockRotation();
+                                BalanduinoActivity.mChatService.write(BalanduinoActivity.sendIMUValues + BalanduinoActivity.mSensorFusion.pitch + ',' + BalanduinoActivity.mSensorFusion.roll + ";");
+                                mButton.setText(R.string.sendingData);
+                            } else {
+                                unlockRotation();
+                                BalanduinoActivity.mChatService.write(BalanduinoActivity.sendStop);
+                                mButton.setText(R.string.notSendingData);
+                            }
+                        }
+                    } else {
+                        mButton.setText(R.string.button);
+                        if (BalanduinoActivity.currentTabSelected == ViewPagerAdapter.IMU_FRAGMENT && BalanduinoActivity.joystickReleased)
+                            CustomViewPager.setPagingEnabled(true);
+                    }
+                }
+            }
+        };
+        mHandler.postDelayed(mRunnable, 50); // Update IMU data every 50ms
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void lockRotation() {
+        if (getResources().getBoolean(R.bool.isTablet)) { // Check if the layout can rotate
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2)
+                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED); // Lock screen orientation so it doesn't rotate
+            else { // Lock rotation manually - source: http://blogs.captechconsulting.com/blog/eric-miles/programmatically-locking-android-screen-orientation
+                int rotation = BalanduinoActivity.getRotation();
+                int lock;
+
+                if (rotation == Surface.ROTATION_90) // Landscape
+                    lock = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                else if (rotation == Surface.ROTATION_180) // Reverse Portrait
+                    lock = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+                else if (rotation == Surface.ROTATION_270) // Reverse Landscape
+                    lock = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+                else
+                    lock = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                getActivity().setRequestedOrientation(lock);
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void unlockRotation() {
+        if (getResources().getBoolean(R.bool.isTablet)) { // Check if the layout can rotate
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2)
+                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_USER); // Unlock screen orientation
+            else
+                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR); // Unlock screen orientation
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mHandler.removeCallbacks(mRunnable);
+        unlockRotation();
+    }
+}
